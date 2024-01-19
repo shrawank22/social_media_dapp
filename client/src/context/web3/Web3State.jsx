c564444e726c4810bcfa5176489ae5108a6b553d
shrawank22 <shrawan8565@gmail.com>
2024-01-19 12:58:21 +0530

    context-api

---
+++ client/src/context/web3/Web3State.jsx
@@ -0,0 +1,29 @@
+import Web3Context from "./web3Context";
+import { useState } from "react";
+import axios from 'axios'
+
+const Web3State = ({ children }) => {
+    const host = "http://localhost:8080"
+
+    const [todos, setTodos] = useState([])
+
+    const [alert, setAlert] = useState(null);
+
+    function showAlert(type, message) {
+        setAlert({
+            type: type,
+            msg: message
+        });
+        setTimeout(() => {
+            setAlert(null);
+        }, 1500);
+    };
+
+    return (
+        <Web3Context.Provider value={{ alert, showAlert }}>
+            {children}
+        </Web3Context.Provider>
+    )
+
+}
+export default Web3State;
